1ae6439fcdbce28957a65c871ca1b4e6673ec003
Pablo Morrone <ext_pnmorrone@falabella.cl>
2024-08-04 23:46:12 -0300

    add a page for not found response

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,18 @@
+export default async function NotFound() {
+  return (
+    <div className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
+      <div className="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl">
+        <p className="text-6xl md:text-7xl lg:text-9xl font-bold tracking-wider text-gray-300">
+          404
+        </p>
+        <p className="text-2xl md:text-3xl lg:text-5xl font-bold tracking-wider text-gray-500 mt-4">
+          oops! Page not found
+        </p>
+        <p className="text-gray-500 mt-8 py-2 border-y-2 text-center">
+          opps! the page you are looking for does not exist. It might have been
+          moved or deleted.
+        </p>
+      </div>
+    </div>
+  );
+}
